b6a062f002150dc505b4a785b2bc0d562b508f35
Hao <zhao5638@gmail.com>
2026-04-06 15:30:41 +0800

    Show full header navigation on mobile and remove hamburger menu

--- themes/cactus/source/js/main.js
+++ themes/cactus/source/js/main.js
@@ -63,13 +63,6 @@ function setupThemeToggle() {
 $(document).ready(function() {
   setupThemeToggle();
 
-  /**
-   * Shows the responsive navigation menu on mobile.
-   */
-  $("#header > #nav > ul > .icon").click(function() {
-    $("#header > #nav > ul").toggleClass("responsive");
-  });
-
 
   /**
    * Controls the different versions of  the menu in blog post articles 
